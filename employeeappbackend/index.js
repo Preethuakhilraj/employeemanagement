b58ebfab46b7a16d4f1a5250285bc6a00dd23746
Preethuakhilraj <46960642+Preethuakhilraj@users.noreply.github.com>
2024-12-26 16:19:13 +0530

    https added

--- employeeappbackend/index.js
+++ employeeappbackend/index.js
@@ -14,10 +14,18 @@ const adminroute = require('./Routes/adminroute');
 const signuproute = require('./Routes/signuproute');
 const loginroute = require('./Routes/login');
 
-app.use(cors({
-  origin: 'http://employeemanagement-client.vercel.app', // Frontend URL
-  credentials: true, // Allow cookies/auth headers
-}));
+// app.use(cors({
+//   origin: 'http://employeemanagement-client.vercel.app', // Frontend URL
+//   credentials: true, // Allow cookies/auth headers
+// }));
+const corsOptions = {
+  origin: ['https://employeemanagement-client.vercel.app', 'http://localhost:3000'], // Add allowed origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));

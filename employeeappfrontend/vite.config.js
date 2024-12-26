af671b6e5adc94dfbfa46a43c67eff0e6f9ef17c
Preethuakhilraj <46960642+Preethuakhilraj@users.noreply.github.com>
2024-12-26 14:02:36 +0530

    build files updated

--- employeeappfrontend/vite.config.js
+++ employeeappfrontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// https://vitejs.dev/config/
+// Define __dirname for ES Module
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Alias src folder
+    },
+  },
+});
